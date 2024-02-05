bcfc60c992959c38ce93252eccc8d7c6c0e35e9e
Sameer Yadav <sameer@Sameers-MacBook-Air.local>
2024-02-05 19:55:00 +0530

    initial commit

--- routes/fdIntimation.mjs
+++ routes/fdIntimation.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
 import schedule from "node-schedule";
-import fdModel from "../models/master/FdModel.mjs";
+import fdModel from "../models/master/fdModel.mjs";
 
 dotenv.config();
 
